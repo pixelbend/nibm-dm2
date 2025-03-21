133f9f079fd0507933af927dc3e78519b0390fb5
pixelbend <92285161+pixelbend@users.noreply.github.com>
2025-03-21 21:29:05 +0530

    feat: updated ReviewModel and introduce ReviewCollection
    Updated ReviewModel to use ObjectId for Id property and made it public. Added ReviewCollection class for managing MongoDB reviews with a singleton pattern, including index creation for efficient querying. Implemented methods for accessing the reviews collection.

--- UrbanFood/Database/MongoDB/ReviewModel.cs
+++ UrbanFood/Database/MongoDB/ReviewModel.cs
@@ -1,13 +1,14 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace UrbanFood.Database.MongoDB
 {
-    class ReviewModel
+    public class ReviewModel
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public ObjectId Id { get; set; }
 
         [BsonElement("ProductID")]
         public required string ProductID { get; set; }
@@ -20,5 +21,35 @@ namespace UrbanFood.Database.MongoDB
 
         [BsonElement("CreatedAt")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+    public sealed class ReviewCollection
+    {
+        private static readonly Lazy<ReviewCollection> _instance = new Lazy<ReviewCollection>(() => new ReviewCollection());
+
+        private readonly IMongoCollection<ReviewModel> _reviewsCollection;
+
+        private ReviewCollection()
+        {
+            var database = MongoDBConnection.Instance.GetDatabase();
+            _reviewsCollection = database.GetCollection<ReviewModel>("Reviews");
+
+            CreateIndexes();
+        }
+
+        public static ReviewCollection Instance => _instance.Value;
+
+        private void CreateIndexes()
+        {
+            var indexKeys = Builders<ReviewModel>.IndexKeys.Text(r => r.Content);
+            var indexOptions = new CreateIndexOptions { Name = "ContentTextIndex" };
+            _reviewsCollection.Indexes.CreateOne(new CreateIndexModel<ReviewModel>(indexKeys, indexOptions));
+
+            var uniqueIndexKeys = Builders<ReviewModel>.IndexKeys.Ascending(r => r.ProductID).Ascending(r => r.CustomerID);
+            var uniqueIndexOptions = new CreateIndexOptions { Unique = true, Name = "ProductCustomerUniqueIndex" };
+            _reviewsCollection.Indexes.CreateOne(new CreateIndexModel<ReviewModel>(uniqueIndexKeys, uniqueIndexOptions));
+        }
+
+        public IMongoCollection<ReviewModel> GetCollection() => _reviewsCollection;
     }
 }
